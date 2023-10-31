73d2f72fe965928c591d7d13f757ce4e63da36d1
NigelOmondi <nigelomondi62@gmail.com>
2023-10-31 23:18:16 +0300

    Refine user experience

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './main-page';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 registerServiceWorker();
